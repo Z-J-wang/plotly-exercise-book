985156870a0118625e99cfc82c591456d2151469
Jay <Jay040030@outlook.com>
2025-07-15 16:45:29 +0800

    build(tailwind): 优化 Tailwind CSS 配置和样式结构
    - 在 tailwind.css 中添加注释，解释各个部分的作用
    - 在 tailwind.config.js 中禁用全局样式重置，避免样式冲突
    - 调整 corePlugins 配置，只使用组件类样式和工具类样式

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false // 禁用全局样式重置，以避免样式冲突。开启此选项会取消基础样式的引入。本项目仅使用工具类样式和组件类样式，故而禁用全局样式重置。
+  },
   prefix: '',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // tailwind CSS 生效路劲
   theme: {
