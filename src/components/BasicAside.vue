63b41dc3f27e9b58f8e8611412fc7398bab4f8bf
Jay <Jay040030@outlook.com>
2025-07-27 22:21:37 +0800

    feat(components): 重构 BasicAside 组件并添加 OptionsAside 组件
    - 重构 BasicAside 组件，使用 el-tabs 实现配置、Api 和案例三个标签页
    - 新增 OptionsAside 组件，用于展示配置选项
    - 更新样式，引入 element-plus 样式

--- src/components/BasicAside.vue
+++ src/components/BasicAside.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
 import { Menu as IconMenu } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+import OptionsAside from './OptionsAside.vue'
+
+let activeTab = ref('options')
 
 const nav = [
   { title: 'Scatter plots', icon: IconMenu, to: { name: 'ScatterPlotsView' } },
@@ -11,12 +15,28 @@ const nav = [
 </script>
 
 <template>
-  <el-menu router>
-    <el-menu-item v-for="({ title, icon, to }, i) in nav" :index="i.toString()" :key="i" :route="to">
-      <el-icon>
-        <component :is="icon"></component>
-      </el-icon>
-      <template #title>{{ title }}</template>
-    </el-menu-item>
-  </el-menu>
+  <div class="p-2">
+    <el-tabs v-model="activeTab">
+      <el-tab-pane label="配置" name="options">
+        <OptionsAside />
+      </el-tab-pane>
+      <el-tab-pane label="Api" name="api">Api</el-tab-pane>
+      <el-tab-pane label="案例" name="edxample">
+        <el-menu router>
+          <el-menu-item
+            class="pl-0 ml-0"
+            v-for="({ title, icon, to }, i) in nav"
+            :index="i.toString()"
+            :key="i"
+            :route="to"
+          >
+            <el-icon>
+              <component :is="icon"></component>
+            </el-icon>
+            <template #title>{{ title }}</template>
+          </el-menu-item>
+        </el-menu>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './css/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import './css/main.css'
 import App from './App.vue'
 import router from './router'
 
