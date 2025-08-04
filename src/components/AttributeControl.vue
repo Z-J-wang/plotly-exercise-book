9ba938a6f46e129174445ba5618a754376b28dfb
Jay <Jay040030@outlook.com>
2025-08-04 15:15:37 +0800

    feat(AttributeControl): 增加数字和选择类型属性控制
    - 添加了对数字类型属性的控制，使用 el-input-number 组件
    - 添加了对选择类型属性的控制，使用 el-select 组件
    - 为组件增加了 min、max、step 和 options 属性

--- src/components/AttributeControl.vue
+++ src/components/AttributeControl.vue
@@ -2,13 +2,11 @@
 import type { PropType } from 'vue'
 
 defineProps({
-  options: {
-    type: Object
-  },
-  type: {
-    type: String as PropType<Attribute.Type>,
-    required: true
-  }
+  options: { type: [], default: () => [] },
+  type: { type: String as PropType<Attribute.Type>, required: true },
+  min: { type: Number, default: Number.MIN_SAFE_INTEGER },
+  max: { type: Number, default: Number.MAX_SAFE_INTEGER },
+  step: { type: Number, default: 1 }
 })
 const modelValue = defineModel<any>()
 </script>
@@ -16,6 +14,7 @@ const modelValue = defineModel<any>()
 <template>
   <div class="attribute-control text-right" style="width: 200px">
     <el-input v-if="type === 'string'" v-model="modelValue" placeholder="请输入内容" />
+    <el-input-number v-else-if="type === 'number'" v-model="modelValue" :min="min" :max="max" :step="step" />
     <el-switch
       v-else-if="type === 'boolean'"
       v-model="modelValue"
@@ -24,5 +23,8 @@ const modelValue = defineModel<any>()
       inactive-text="Close"
     />
     <el-color-picker v-else-if="type === 'color'" v-model="modelValue" />
+    <el-select v-else-if="type === 'select'" v-model="modelValue" placeholder="Select">
+      <el-option v-for="item in options" :key="item" :label="item" :value="item" />
+    </el-select>
   </div>
 </template>
