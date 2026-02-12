bd01b9622a248fa97a37d8b433a1600c7ac534d2
Jay <Jay040030@outlook.com>
2026-02-12 14:23:45 +0800

    refactor(dal): 重构trace.stack.ts中的堆叠配置属性
    将TraceStack类中的stackgroup、groupnorm、orientation和stackgaps属性
    重构为独立的Attribute子类，提高代码结构清晰度和可维护性。
    
    引入lodash的merge函数来简化初始化器合并逻辑，
    移除了重复的parent和initialConfig传递参数。

--- dal/plotly.config/traces/trace.stack.ts
+++ dal/plotly.config/traces/trace.stack.ts
@@ -1,11 +1,22 @@
 import Attribute from 'entity/attribute'
 import AttributeController from 'entity/attribute.controller'
+import { merge } from 'lodash'
 
 export default class TraceStack {
   constructor(parent: Attribute, initialConfig: PlotlyConfig) {
-    parent.addChild(
-      new Attribute('stackgroup', 'string', {
-        parent,
+    parent.addChild(new TraceStackgroup({ options: { parent, initialConfig } }))
+    parent.addChild(new TraceGroupnorm({ options: { parent, initialConfig } }))
+    parent.addChild(new TraceOrientation({ options: { parent, initialConfig } }))
+    parent.addChild(new TraceStackgaps({ options: { parent, initialConfig } }))
+  }
+}
+
+export class TraceStackgroup extends Attribute {
+  constructor(initializer: Attribute.Initializer) {
+    const defaultInitializer = {
+      name: 'stackgroup',
+      type: 'string',
+      options: {
         description: {
           type: 'string',
           value:
@@ -20,78 +31,81 @@ export default class TraceStack {
             '<li>存在多个堆叠分组或部分轨迹堆叠而部分未堆叠的情况下，若填充关联的轨迹未连续排列，后续轨迹将在绘制顺序中被下移。</li>' +
             '</ul>'
         },
-        controller: new AttributeController({ type: 'string', default: null, value: 'group1', disabled: true }),
-        initialConfig: initialConfig
-      })
-    )
+        controller: new AttributeController({ type: 'string', default: null, value: 'group1', disabled: true })
+      }
+    }
+    super(merge(defaultInitializer, initializer))
+  }
+}
 
-    parent.addChild(
-      new Attribute(
-        'groupnorm',
-        { type: 'enum', value: ['', 'fraction', 'percent'] },
-        {
-          parent,
-          description: {
-            type: 'string',
-            value:
-              '设置堆叠分组各项占比的计算方式。' +
-              '可选值：' +
-              '<ul>' +
-              "<li><code>''</code> - 默认值。每个条形图在每个位置上的值直接相加堆叠。</li>" +
-              '<li><code>fraction</code> - 每个条形图在每个位置上的值会除以该位置上所有条形图值的总和。</li>' +
-              '<li><code>percent</code> - 每个条形图在每个位置上的值会除以该位置上所有条形图值的总和，并乘以100。</li>' +
-              '</ul>'
-          },
-          controller: new AttributeController({
-            type: 'select',
-            default: "''",
-            options: ['', 'fraction', 'percent']
-          }),
-          initialConfig: initialConfig
-        }
-      )
-    )
+export class TraceGroupnorm extends Attribute {
+  constructor(initializer: Attribute.Initializer) {
+    const defaultInitializer = {
+      name: 'groupnorm',
+      type: { type: 'enum', value: ['', 'fraction', 'percent'] },
+      options: {
+        description: {
+          type: 'string',
+          value:
+            '设置堆叠分组各项占比的计算方式。' +
+            '可选值：' +
+            '<ul>' +
+            "<li><code>''</code> - 默认值。每个条形图在每个位置上的值直接相加堆叠。</li>" +
+            '<li><code>fraction</code> - 每个条形图在每个位置上的值会除以该位置上所有条形图值的总和。</li>' +
+            '<li><code>percent</code> - 每个条形图在每个位置上的值会除以该位置上所有条形图值的总和，并乘以100。</li>' +
+            '</ul>'
+        },
+        controller: new AttributeController({
+          type: 'select',
+          default: "''",
+          options: ['', 'fraction', 'percent']
+        })
+      }
+    }
+    super(merge(defaultInitializer, initializer))
+  }
+}
 
-    parent.addChild(
-      new Attribute(
-        'orientation',
-        { type: 'enum', value: ['v', 'h'] },
-        {
-          parent,
-          description: {
-            type: 'string',
-            value: '设置轨迹的显示方向。默认为 <code>v</code>。'
-          },
-          controller: new AttributeController({ type: 'select', default: 'v', options: ['v', 'h'] }),
-          initialConfig: initialConfig
-        }
-      )
-    )
+export class TraceOrientation extends Attribute {
+  constructor(initializer: Attribute.Initializer) {
+    const defaultInitializer = {
+      name: 'orientation',
+      type: { type: 'enum', value: ['v', 'h'] },
+      options: {
+        description: {
+          type: 'string',
+          value: '设置轨迹的显示方向。默认为 <code>v</code>。'
+        },
+        controller: new AttributeController({ type: 'select', default: 'v', options: ['v', 'h'] })
+      }
+    }
+    super(merge(defaultInitializer, initializer))
+  }
+}
 
-    parent.addChild(
-      new Attribute(
-        'stackgaps',
-        { type: 'enum', value: ['infer zero', 'interpolate'] },
-        {
-          parent,
-          description: {
-            type: 'string',
-            value:
-              '设置堆叠组中缺失值如何处理。' +
-              '可选值：' +
-              '<ul>' +
-              '<li><code>infer zero</code> - 默认值。将缺失值填充为零。</li>' +
-              '<li><code>interpolate</code> - 将缺失值填充为插值。</li>' +
-              '</ul>'
-          },
-          controller: new AttributeController({
-            type: 'select',
-            default: 'infer zero',
-            options: ['infer zero', 'interpolate']
-          }),
-          initialConfig: initialConfig
-        }
-      )
-    )
+export class TraceStackgaps extends Attribute {
+  constructor(initializer: Attribute.Initializer) {
+    const defaultInitializer = {
+      name: 'stackgaps',
+      type: { type: 'enum', value: ['infer zero', 'interpolate'] },
+      options: {
+        description: {
+          type: 'string',
+          value:
+            '设置堆叠组中缺失值如何处理。' +
+            '可选值：' +
+            '<ul>' +
+            '<li><code>infer zero</code> - 默认值。将缺失值填充为零。</li>' +
+            '<li><code>interpolate</code> - 将缺失值填充为插值。</li>' +
+            '</ul>'
+        },
+        controller: new AttributeController({
+          type: 'select',
+          default: 'infer zero',
+          options: ['infer zero', 'interpolate']
+        })
+      }
+    }
+    super(merge(defaultInitializer, initializer))
   }
 }
